f9fd884856d744a167d5f22983f0a2b8d083e082
m0opha <142889788+m0opha@users.noreply.github.com>
2024-12-21 02:26:20 -0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,6 @@
+virtualev .env
+source .env/bin/activate
+pip install pyinstaller
 pyinstaller --onefile -i NONE handler_workdir.py
 cp dist/handler_workdir ~/bin
 bash ./scripts/create_bash_function.sh
-
-
